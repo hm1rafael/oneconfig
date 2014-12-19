0c5fe6c0b1eb06a3e059c8c8023461634fd33590
rafaelmattos <rafael.mattos@softplan.com.br>
2014-12-19 14:02:10 -0200

    commit

---
+++ src/main/java/com/newgig/repository/MapDBCloudEnviromentRepository.java
@@ -0,0 +1,21 @@
+package com.newgig.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.config.Environment;
+import org.springframework.cloud.config.server.EnvironmentRepository;
+import org.springframework.stereotype.Component;
+
+import com.newgig.dao.mapdb.ApplicationDataSourceDAOImpl;
+
+@Component
+public class MapDBCloudEnviromentRepository implements EnvironmentRepository {
+
+	@Autowired
+	private ApplicationDataSourceDAOImpl applicationDatasourceDAO;
+
+	@Override
+	public Environment findOne(String aplicacao, String sufixo, String versao) {
+		return null;
+	}
+
+}
